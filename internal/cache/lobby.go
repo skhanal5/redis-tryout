e52d820001639537881f04de876a309a7dc58d72
Subodh <skhanal5@terpmail.umd.edu>
2025-04-13 20:22:44 -0400

    Pull out redis client implementation outside of lobby implementation. Define common interface for all cache related behavior

---
+++ internal/cache/lobby.go
@@ -0,0 +1,48 @@
+package cache
+
+import (
+	"context"
+	"fmt"
+)
+
+type LobbyManager interface {
+	CreateLobby(ctx context.Context, name string) error
+	JoinLobby(ctx context.Context, player string, lobby string) error
+	LeaveLobby(ctx context.Context, player string, lobby string) error
+}
+
+func (c Cache) CreateLobby(ctx context.Context, name string) error {
+	lobbyKey := fmt.Sprintf("lobby:%s", name)
+	res, err := c.Client.SAdd(ctx, lobbyKey).Result()
+	if err != nil {
+		return err
+	}
+	fmt.Print(res)
+	return nil
+}
+
+func (c Cache) JoinLobby(ctx context.Context, player string, lobby string) error {
+	lobbyKey := fmt.Sprintf("lobby:%s", lobby)
+	playerItem := fmt.Sprintf("player:%s", player)
+
+	res, err := c.Client.SAdd(ctx, lobbyKey, playerItem).Result()
+	if err != nil {
+		return err
+	}
+	fmt.Print(res)
+	return nil
+}
+
+
+func (c Cache) LeaveLobby(ctx context.Context, player string, lobby string) error {
+	lobbyKey := fmt.Sprintf("lobby:%s", lobby)
+	playerItem := fmt.Sprintf("player:%s", player)
+	
+	res, err := c.Client.SRem(ctx, lobbyKey, playerItem).Result()
+	if err != nil {
+		return err
+	}
+	fmt.Print(res)
+	return nil
+}
+
